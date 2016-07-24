484b19f5a7c2cccdffd220b2a1b4961718d463e5
Po Chen <skycper@gmail.com>
2016-07-24 23:56:12 +0800

    Add jquery and bootstrap. Create Nav component.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ require('laravel-elixir-livereload');
 elixir.config.sourcemaps = false;
 
 elixir(function(mix) {
-    mix.sass('app.scss')
-       .browserify('app.js')
-       .livereload();
+	mix.sass('app.scss')
+		.browserify('app.js')
+		.livereload();
 });
--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -1,16 +1,16 @@
-import React, {
-  Component
-} from 'react'
-import {
-  Link,
-  IndexLink
-} from 'react-router'
+import React, { Component } from 'react'
+import { Link, IndexLink } from 'react-router'
+import Nav from './Nav'
+
+var $ = require('jquery');
 
 class App extends Component {
 
   render() {
     return (
       <div>
+        <Nav />
+
         <h1><IndexLink to="/">Drama</IndexLink></h1>
 
         <ul>
@@ -19,7 +19,7 @@ class App extends Component {
 
         {this.props.children}
       </div>
-    )
+    );
   }
 
 }
